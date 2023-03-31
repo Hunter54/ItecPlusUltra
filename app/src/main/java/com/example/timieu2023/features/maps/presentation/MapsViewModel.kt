8db331f2c514054062662eba2c32435b32064a6a
Vlad <ricean.vlad7@gmail.com>
2023-03-31 21:11:09 +0300

    Add custom theme/ dynamic theme

--- app/src/main/java/com/example/timieu2023/features/maps/presentation/MapsViewModel.kt
+++ app/src/main/java/com/example/timieu2023/features/maps/presentation/MapsViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.timieu2023.features.maps.presentation
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MapsViewModel @Inject constructor(
 
-) {
+): ViewModel() {
 
 }
