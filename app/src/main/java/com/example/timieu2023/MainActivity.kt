ce1dc3b78983531b48cb141efeaacb3dd8639fa7
CatalinVanciu <73790145+CatalinVanciu@users.noreply.github.com>
2023-03-31 21:16:53 +0300

    - create splash screen

--- app/src/main/java/com/example/timieu2023/MainActivity.kt
+++ app/src/main/java/com/example/timieu2023/MainActivity.kt
@@ -1,43 +1,52 @@
 package com.example.timieu2023
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.timieu2023.ui.theme.TimiEu2023Theme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+    private var keepSplashOpened = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition{ keepSplashOpened }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
         setContent {
             TimiEu2023Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreen(
+                    onDataLoaded = {
+                        keepSplashOpened = false
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SplashScreen(onDataLoaded: () -> Unit) {
+    var fakeLoading by remember { mutableStateOf(true) }
+    
+    LaunchedEffect(key1 = Unit) {
+        delay(2000)
+        fakeLoading = false
+        onDataLoaded()
+    }
+
+    if(!fakeLoading) {
+        MainScreen()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TimiEu2023Theme {
-        Greeting("Android")
-    }
+fun MainScreen() {
+    Text(text = "Hello world!")
 }
