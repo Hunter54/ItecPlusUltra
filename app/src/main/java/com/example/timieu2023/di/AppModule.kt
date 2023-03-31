764356c450ea70883f1e849a7fc95d98dfa583a1
Vlad <ricean.vlad7@gmail.com>
2023-03-31 20:28:46 +0300

    Add DI

--- app/src/main/java/com/example/timieu2023/di/AppModule.kt
+++ app/src/main/java/com/example/timieu2023/di/AppModule.kt
@@ -1,4 +1,19 @@
 package com.example.timieu2023.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+//    @Singleton
+//    @Provides
+//    fun provideEventsRepository(
+//        dataSource: EventsDataSource
+//    ): EventsRepository = EventsRepositoryImpl(dataSource)
+
 }
---
+++ app/src/main/java/com/example/timieu2023/features/maps/presentation/MapsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.timieu2023.features.maps.presentation
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MapsViewModel @Inject constructor(
+
+) {
+
+}
