14320849e3c6b1ceb4ef446b3aca922ca6b850e9
Vlad <ricean.vlad7@gmail.com>
2023-04-01 13:40:03 +0300

    Search bar done

--- app/src/main/java/com/example/timieu2023/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/timieu2023/features/home/presentation/HomeScreen.kt
@@ -1,39 +1,25 @@
 package com.example.timieu2023.features.home.presentation
 
-import android.widget.SearchView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.timieu2023.R
 
@@ -42,14 +28,23 @@ fun HomeTabRoute(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    HomeTabScreen()
+    val homeState by viewModel.state.collectAsState()
+
+    HomeTabScreen(
+        onQueryChange = { query ->
+            viewModel.onSearchQueryChange(query)
+        },
+        homeState.query,
+        modifier
+    )
 }
 
 @Composable
 fun HomeTabScreen(
+    onQueryChange: (String) -> Unit,
+    query: String,
     modifier: Modifier = Modifier
 ) {
-    val textState = remember { mutableStateOf(TextFieldValue("")) }
 
     Column {
         TopWelcomeTab(
@@ -58,10 +53,9 @@ fun HomeTabScreen(
         HomeSearchField(
             onSearch = {},
             onActiveChange = {},
-            onQueryChange = {},
-            query = ""
+            onQueryChange = onQueryChange,
+            query = query
         )
-//        SearchView(state = textState)
     }
 }
 
@@ -91,74 +85,26 @@ fun HomeSearchField(
     onSearch: (String) -> Unit,
     onActiveChange: (Boolean) -> Unit,
     onQueryChange: (String) -> Unit,
-    query: String
+    query: String,
+    modifier: Modifier = Modifier
 ) {
     SearchBar(
         query = query,
         onQueryChange = onQueryChange,
         onSearch = onSearch,
-        active = true,
+        active = false,
         onActiveChange = onActiveChange,
         enabled = true,
-        placeholder = {},
-        leadingIcon = {},
         trailingIcon = {
             Icon(imageVector = Icons.Filled.Search, contentDescription = null)
         },
         tonalElevation = 16.dp,
+        shape = RoundedCornerShape(16.dp),
+        modifier = modifier.padding(start = 16.dp, end = 16.dp)
     ) {
 
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SearchView(state: MutableState<TextFieldValue>) {
-    TextField(
-        value = state.value,
-        onValueChange = { value ->
-            state.value = value
-        },
-        modifier = Modifier.fillMaxWidth(),
-        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
-        leadingIcon = {
-            Icon(
-                Icons.Default.Search,
-                contentDescription = "",
-                modifier = Modifier
-                    .padding(15.dp)
-                    .size(24.dp)
-            )
-        },
-        trailingIcon = {
-            if (state.value != TextFieldValue("")) {
-                IconButton(
-                    onClick = {
-                        state.value =
-                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
-                    }
-                ) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = "",
-                        modifier = Modifier
-                            .padding(15.dp)
-                            .size(24.dp)
-                    )
-                }
-            }
-        },
-        singleLine = true,
-        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
-        colors = TextFieldDefaults.textFieldColors(
-            cursorColor = Color.White,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        )
-    )
-}
-
 
 @Composable
 fun HomeTopQuestion() {
--- app/src/main/java/com/example/timieu2023/features/home/presentation/HomeViewModel.kt
+++ app/src/main/java/com/example/timieu2023/features/home/presentation/HomeViewModel.kt
@@ -2,10 +2,29 @@ package com.example.timieu2023.features.home.presentation
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
 
 ): ViewModel() {
+
+    private val _state = MutableStateFlow<HomeViewState>(HomeViewState(query = "Search destination"))
+    val state: StateFlow<HomeViewState> = _state
+
+    fun onSearchQueryChange(changedQuery: String) {
+        _state.update {
+            it.copy(query = changedQuery)
+        }
+    }
+
 }
+
+data class HomeViewState(
+//    val listAllEvent: List<HomeViewData> = emptyList(),
+//    val isNetworkAvailable: Boolean? = null,
+    val query: String
+)
