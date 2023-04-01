72a14de1b86719e2ce90e04e4b9e65cba4674715
Vlad <ricean.vlad7@gmail.com>
2023-04-01 13:17:35 +0300

    Home screen wip

--- app/src/main/java/com/example/timieu2023/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/timieu2023/features/home/presentation/HomeScreen.kt
@@ -1,19 +1,39 @@
 package com.example.timieu2023.features.home.presentation
 
+import android.widget.SearchView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.timieu2023.R
 
@@ -29,26 +49,118 @@ fun HomeTabRoute(
 fun HomeTabScreen(
     modifier: Modifier = Modifier
 ) {
-    TopWelcomeTab(
-        modifier
-    )
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
+
+    Column {
+        TopWelcomeTab(
+            modifier
+        )
+        HomeSearchField(
+            onSearch = {},
+            onActiveChange = {},
+            onQueryChange = {},
+            query = ""
+        )
+//        SearchView(state = textState)
+    }
 }
 
 @Composable
 fun TopWelcomeTab(
     modifier: Modifier = Modifier
 ) {
-    Row {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.mosulica), contentDescription = null,
-            modifier.padding(16.dp)
+            modifier.padding(start = 16.dp, top = 12.dp, end = 12.dp, bottom = 12.dp)
         )
-        Column {
-            Row {
-                Text(text = "Welcome Home")
-                Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
-            }
-            Text("Sarah Good")
-        }
+        Text(text = "Welcome Back")
+        Icon(
+            imageVector = Icons.Filled.Favorite, contentDescription = null,
+            modifier = modifier.padding(start = 4.dp)
+        )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeSearchField(
+    onSearch: (String) -> Unit,
+    onActiveChange: (Boolean) -> Unit,
+    onQueryChange: (String) -> Unit,
+    query: String
+) {
+    SearchBar(
+        query = query,
+        onQueryChange = onQueryChange,
+        onSearch = onSearch,
+        active = true,
+        onActiveChange = onActiveChange,
+        enabled = true,
+        placeholder = {},
+        leadingIcon = {},
+        trailingIcon = {
+            Icon(imageVector = Icons.Filled.Search, contentDescription = null)
+        },
+        tonalElevation = 16.dp,
+    ) {
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchView(state: MutableState<TextFieldValue>) {
+    TextField(
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        state.value =
+                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
+        colors = TextFieldDefaults.textFieldColors(
+            cursorColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+
+@Composable
+fun HomeTopQuestion() {
+
 }
